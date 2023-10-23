ef892607f316b925a4df7394cad959445d41f108
snayderPinto88-58 <54647990+snayderPinto88-58@users.noreply.github.com>
2023-10-23 10:43:17 +0200

    agregando los modelos

---
+++ usuarioVideojuego/Data/Migrations/20231023080727_InitialMigration.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace usuarioVideojuego.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "usuarios",
+                columns: table => new
+                {
+                    Id_usuario = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nombreUsuario = table.Column<string>(type: "text", nullable: false),
+                    genero = table.Column<string>(type: "text", nullable: false),
+                    edad = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarios", x => x.Id_usuario);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Emails",
+                columns: table => new
+                {
+                    Id_email = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    Id_usuario = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emails", x => x.Id_email);
+                    table.ForeignKey(
+                        name: "FK_Emails_usuarios_Id_usuario",
+                        column: x => x.Id_usuario,
+                        principalTable: "usuarios",
+                        principalColumn: "Id_usuario",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "videoJuegos",
+                columns: table => new
+                {
+                    Id_videojuego = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nombreVideojuego = table.Column<string>(type: "text", nullable: false),
+                    tipoDePago = table.Column<string>(type: "text", nullable: false),
+                    EsGrupal = table.Column<bool>(type: "boolean", nullable: false),
+                    Tipo = table.Column<string>(type: "text", nullable: false),
+                    Id_usuario = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_videoJuegos", x => x.Id_videojuego);
+                    table.ForeignKey(
+                        name: "FK_videoJuegos_usuarios_Id_usuario",
+                        column: x => x.Id_usuario,
+                        principalTable: "usuarios",
+                        principalColumn: "Id_usuario",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Emails_Id_usuario",
+                table: "Emails",
+                column: "Id_usuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_videoJuegos_Id_usuario",
+                table: "videoJuegos",
+                column: "Id_usuario");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Emails");
+
+            migrationBuilder.DropTable(
+                name: "videoJuegos");
+
+            migrationBuilder.DropTable(
+                name: "usuarios");
+        }
+    }
+}
---
+++ usuarioVideojuego/Models/Email.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace usuarioVideojuego.Models
+{
+    public class Email
+    {
+        [Key]
+        public int Id_email { get; set; }
+        public string email { get; set; }
+
+        [ForeignKey("Usuario")]
+        public int Id_usuario { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+
+}
